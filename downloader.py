c644d9b6f9ed1187040a862c8a9cdf2c0bb9339b
SunburntStorm48 <109062370+SunburntStorm48@users.noreply.github.com>
2023-08-21 12:57:00 +0800

    Script

---
+++ downloader.py
@@ -0,0 +1,32 @@
+#path of the files can be found in generateMakefile.js
+import urllib.request
+import time
+import os
+imglist = [
+
+]
+#list of files
+urlprefix = 'https://cdn.picrew.me/app/image_maker/'
+prefix = 'C:/Downloads/pytemp/'
+despath = 'C:/Downloads/pytemp/168503/' #path to save the files
+if os.path.exists(prefix) == False:
+    os.mkdir(prefix)
+if os.path.exists(despath) == False:    
+    os.mkdir(despath)
+
+for i in imglist:
+    url = urlprefix + i
+    files = prefix + i
+    urlx = str(url)
+    filesx = str(files)
+    
+    string = i
+    string = string[0:13]
+    ipath = prefix + string
+    
+    print (ipath)
+    if os.path.exists(ipath) == False:
+        os.mkdir(ipath)
+    urllib.request.urlretrieve(urlx, filesx)
+    print('Downloaded', urlx)
+    print('Saved', filesx)
