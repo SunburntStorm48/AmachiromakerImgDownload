bc64dd5a9ac61cd2b9a695b50acb056c45947455
SunburntStorm48 <109062370+SunburntStorm48@users.noreply.github.com>
2023-08-22 12:17:01 +0800

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -1,6 +1,7 @@
 import urllib.request
 import time
 import os
+import sys
 imglist = [
 
 ]
@@ -8,12 +9,16 @@ imglist = [
 urlprefix = 'https://cdn.picrew.me/app/image_maker/'
 prefix = 'C:/Downloads/pytemp/'
 despath = 'C:/Downloads/pytemp/168503/' #path to save the files
+length = len(imglist)
+m = 0
+k = 0
+
 if os.path.exists(prefix) == False:
     os.mkdir(prefix)
 if os.path.exists(despath) == False:    
     os.mkdir(despath)
 
-for i in imglist:
+def down(i):
     url = urlprefix + i
     files = prefix + i
     urlx = str(url)
@@ -22,10 +27,18 @@ for i in imglist:
     string = i
     string = string[0:13]
     ipath = prefix + string
-    
-    print (ipath)
+
     if os.path.exists(ipath) == False:
         os.mkdir(ipath)
     urllib.request.urlretrieve(urlx, filesx)
-    print('Downloaded', urlx)
-    print('Saved', filesx)
+
+print ('[Downloading', length, 'files]')
+
+for i in imglist:
+    down(i)
+    m += 1
+    k = int((m / length) * 100)
+
+    print('\r', end='')
+    print('Download progress: {}% '.format(k), '▋' * (k // 2), m,'/',length, end='')
+    sys.stdout.flush()
